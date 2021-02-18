923dad07c9f71a62d9a1d8c99046cc2d8e08b65d
Marcelo Ribeiro <marcelittle@pop-os.localdomain>
2021-02-18 18:51:53 -0300

    project base

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useCallback, useState } from "react";
+// Import the dropzone component
+import Dropzone from "./Dropzone";
+import ImageList from "./ImageList";
+import cuid from "cuid";
+
+import "./App.css";
+
+function App() {
+  // Create a state called images using useState hooks and pass the initial value as empty array
+  const [images, setImages] = useState([]);
+
+  const onDrop = useCallback((acceptedFiles) => {
+    // Loop through accepted files
+    acceptedFiles.map((file) => {
+      // Initialize FileReader browser API
+      const reader = new FileReader();
+      // onload callback gets called after the reader reads the file data
+      reader.onload = function (e) {
+        // add the image into the state. Since FileReader reading process is asynchronous, its better to get the latest snapshot state (i.e., prevState) and update it.
+        setImages((prevState) => [
+          ...prevState,
+          { id: cuid(), src: e.target.result },
+        ]);
+      };
+      // Read the file as Data URL (since we accept only images)
+      reader.readAsDataURL(file);
+      return file;
+    });
+  }, []);
+  return (
+    <main className="App">
+      <h1 className="text-center">Drag and Drop Example</h1>
+      <Dropzone onDrop={onDrop} accept={"image/*"} />
+      <ImageList images={images} />
+    </main>
+  );
+}
+
+export default App;
